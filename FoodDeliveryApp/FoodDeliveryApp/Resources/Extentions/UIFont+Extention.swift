d70c4b6e785b60f2c891082d3d77e4cb91dfdaf6
Max Kuznetsov <kuzma-2000@mail.riu>
2024-07-30 22:13:12 +0300

    eded fonts enums

--- FoodDeliveryApp/FoodDeliveryApp/Resources/Extentions/UIFont+Extention.swift
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/Extentions/UIFont+Extention.swift
@@ -14,9 +14,64 @@ extension UIFont {
                 return UIFont(name: Constants.Roboto.black, size: size)!
             }
         }
+        enum blackItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.blackItalic, size: size)!
+            }
+        }
+        enum bold {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.bold, size: size)!
+            }
+        }
+
+        enum boldItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.boldItalic, size: size)!
+            }
+        }
+        enum italic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.italic, size: size)!
+            }
+        }
+        enum light {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.light, size: size)!
+            }
+        }
+        enum lightItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.lightItalic, size: size)!
+            }
+        }
+        enum medium {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.medium, size: size)!
+            }
+        }
+        enum mediumItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.mediumItalic, size: size)!
+            }
+        }
+        enum regular {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.regular, size: size)!
+            }
+        }
+        enum thin {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.thin, size: size)!
+            }
+        }
+        enum thinItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.thinItalic, size: size)!
+            }
+        }
     }
 }
-
 private extension UIFont {
     enum Constants {
         enum Roboto {
